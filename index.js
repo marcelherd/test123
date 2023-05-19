5021176b8d750745ba8013d894ddb68b89c7d568
Marcel Herd <me@marcelherd.com>
2023-05-19 17:46:18 +0200

    Add Helmet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 
 const { characters } = require("./characters");
 const { getRandomQuote } = require("./quotes");
@@ -6,6 +7,8 @@ const { getRandomQuote } = require("./quotes");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
 
 app.get("/api/characters", (req, res) => res.json(characters));
 app.get("/api/quotes/random", (req, res) => res.json(getRandomQuote()));
